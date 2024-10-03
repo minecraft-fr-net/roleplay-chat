b280db67d6ba8d326da8ccf5ce0290db79d9bbe0
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-10-03 14:20:09 +0200

    ✨ Support message can be receive only by OP (admin)

--- src/main/java/net/minecraftfr/roleplaychat/chatTypeMessage/SupportMessage.java
+++ src/main/java/net/minecraftfr/roleplaychat/chatTypeMessage/SupportMessage.java
@@ -1,5 +1,9 @@
 package net.minecraftfr.roleplaychat.chatTypeMessage;
 
+import org.spongepowered.asm.mixin.Overwrite;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+
 public class SupportMessage extends MessageType {
   public static final int RADIUS = 0;
   public static final int COLOR = 0xFF99CC;
@@ -8,5 +12,20 @@ public class SupportMessage extends MessageType {
 
   public SupportMessage(String message) {
     super(message, RADIUS, COLOR, CHARACTER);
+  }
+
+  @Overwrite
+  public void sendMessage(ServerPlayerEntity sender, ServerPlayerEntity receiver) {
+    if (canReceive(receiver)) {
+      super.sendMessage(sender, receiver);
+    }
+  }
+
+  /*
+   * Return if receiver can receipt the message
+   * only if the receiver is OP (admin)
+   */
+  public boolean canReceive(ServerPlayerEntity receiver) {
+    return receiver.hasPermissionLevel(2);
   }
 }
